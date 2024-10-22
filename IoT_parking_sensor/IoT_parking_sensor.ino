74c14f19828060c5b9658d923b5d5ff25d42b0e1
Federico Melis <70884674+fedemelis@users.noreply.github.com>
2024-10-22 13:56:53 +0200

    add latitude and longitude to parking slot

--- IoT_parking_sensor/IoT_parking_sensor.ino
+++ IoT_parking_sensor/IoT_parking_sensor.ino
@@ -18,15 +18,33 @@ struct Sensor{
 
 
 struct Slot{
-  uint8_t zone_;
-  uint8_t id_;
-  Sensor s1_;
-  Sensor s2_;
+  uint8_t zone_; // parking zone
+  uint8_t id_; // parking id
+  double lat_; // parking latitude
+  double lon_; // parking longitude
+  Sensor s1_; // first sensor
+  Sensor s2_; // second sensor
   ParkingState currentState;
   ParkingState futureState;
 
-  Slot(uint8_t zone, uint8_t id, uint8_t pin1, uint8_t pin2) : 
-    zone_(zone), id_(id), s1_(pin1), s2_(pin2), currentState(free_), futureState(free_) {}
+  /**
+  * @brief Constructor for a slot.
+  *
+  *
+  * @param zone The zone that the slot belongs to.
+  * @param id The unique id wrt zone.
+  * @param lat Latitude of parking slot.
+  * @param lon Longitude of parking slot.
+  * @param pin1 The pin of the first sensor.
+  * @param pin2 The pin of the second sensor.
+  * 
+  *
+  */
+  Slot(uint8_t zone, uint8_t id, double lat, double lon, uint8_t pin1, uint8_t pin2) : 
+    zone_(zone), id_(id), lat_(lat), lon_(lon), s1_(pin1), s2_(pin2), currentState(free_), futureState(free_) {}
+
+  // empty constructor
+  Slot() : zone_(0), id_(0), lat_(0), lon_(0), s1_(0), s2_(0) {}
 };
 
 uint64_t startMillis;
@@ -36,7 +54,7 @@ bool timeout(uint64_t duration){
 }
 
 // test slots
-Slot slots[3] = {Slot(1, 0, 2, 3), Slot(1, 1, 4, 5), Slot(1, 2, 6, 7)};
+Slot slots[3] = {Slot(1, 0, 44.0, 44.0, 2, 3), Slot(1, 1, 44.0, 44.0, 4, 5), Slot(1, 2, 44.0, 44.0, 6, 7)};
 
 // led rgb as actuator
 uint8_t greenPin = 12;
@@ -63,16 +81,30 @@ void output(uint8_t out){
 
 void setup() {
     Serial.begin(9600);
+    // i should retrive from the bridge the slots
+
 }
 
 void loop() {
-
     for(size_t i = 0; i < sizeof(slots) / sizeof(Slot); ++i){
-        Slot s = slots[i];
+        Slot& s = slots[i];
         
         // reading input
-        s.s1_.state = digitalRead(s.s1_.pin_);
-        s.s2_.state = digitalRead(s.s2_.pin_);
+        int state1 = digitalRead(s.s1_.pin_);
+        int state2 = digitalRead(s.s2_.pin_);
+        if (state1 == HIGH) {
+          s.s1_.state = on_;
+        }
+        else if (state1 == LOW) {
+          s.s1_.state = off_;
+        }
+
+        if (state2 == HIGH) {
+          s.s2_.state = on_;
+        }
+        else if (state2 == LOW) {
+          s.s2_.state = off_;
+        }
 
         // state change
         // default behaviour is staying in the current state
@@ -100,6 +132,7 @@ void loop() {
         if(s.currentState != s.futureState){
             if(s.currentState == free_ && s.currentState == requested_){
                 Serial.println("Il parcheggio è richiesto");
+                // start timer
                 startMillis = millis();
             }
             else if(s.currentState == requested_ && s.futureState == taken_){

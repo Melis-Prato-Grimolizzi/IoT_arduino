af2c49649780c20157aa9dd5a93625ef04430fae
Federico Melis <70884674+fedemelis@users.noreply.github.com>
2025-02-22 18:10:49 +0100

    working comms
    added 3 slots
    added timer vector
    added print parameter

--- IoT_parking_sensor/IoT_parking_sensor.ino
+++ IoT_parking_sensor/IoT_parking_sensor.ino
@@ -7,7 +7,7 @@
   un sensore che richiede due pin di digitali (trig e echo) usando
   solo un pin
 */
-#define MAX_DISTANCE 100 // distanza massima pingabile in cm 
+#define MAX_DISTANCE 200 // distanza massima pingabile in cm 
 #define ACTIVATION_TRESH 70 // treshold di attivazione del sensore
 
 // pin per il primo slot
@@ -23,9 +23,7 @@
 #define PIN_SLOT_3_1 9
 #define PIN_SLOT_3_2 8
 
-// pin E220-900T22D
-#define RXD 2
-#define TXD 3 
+
 
 #define DELTA_READING 100
 
@@ -38,9 +36,9 @@ uint8_t leds[3] = {led1, led2, led3};
 #define ZONE 1 // hard coded zone
 
 const uint64_t initRequestRate = 3000; // request rate for init phase
-uint64_t stabilize = 0;
+uint64_t stabilize[3] = {0, 0, 0};
 bool tryInit = true; // to know when to send again init packet
-bool to_stabilize = true;
+bool to_stabilize[3] = {true, true, true};
 
 uint64_t startMillis; // timer var for state changing
 
@@ -112,15 +110,32 @@ NewPing s3_2(PIN_SLOT_3_2, PIN_SLOT_3_2, MAX_DISTANCE);
 Sensor distanceSensor1_1(s1_1);
 Sensor distanceSensor1_2(s1_2);
 
+Sensor distanceSensor2_1(s2_1);
+Sensor distanceSensor2_2(s2_2);
+
+Sensor distanceSensor3_1(s3_1);
+Sensor distanceSensor3_2(s3_2);
+
+/*
+LoRa definition
+
+RXD refers to Arduino's RX pin
+TXD refers to Arduino's TX pin
+
+The comunication is Arduino_1 -> LoRa_1 -> LoRa_2 -> Arduino_2
+*/
+// pin E220-900T22D
+#define RXD 2
+#define TXD 3 
 LoRa_E220 lora(RXD, TXD);
 
 uint64_t delays[3] = {0, 0, 0};
 
 /////////////////////////////////////////////////// test slots
-Slot slots[1] = {
+Slot slots[3] = {
  Slot(ZONE, 1, 44.11, 10.11, distanceSensor1_1, distanceSensor1_2),
- //Slot(ZONE, 2, 44.11, 10.11, s2_1, s2_2),
- //Slot(ZONE, 3, 44.11, 10.11, s3_1, s3_2)
+ Slot(ZONE, 2, 44.11, 10.11, distanceSensor2_1, distanceSensor2_2),
+ Slot(ZONE, 3, 44.11, 10.11, distanceSensor3_1, distanceSensor3_2)
  };
 ///////////////////////////////////////////////////
 
@@ -136,11 +151,17 @@ void setup() {
     Serial.println("Starting LoRa...");
     lora.begin();
     Serial.println("LoRa succesfully started!");
+    ResponseStructContainer c;
+    c = lora.getConfiguration();
+    Serial.println("Get configuration effettuato");
+	  // It's important get configuration pointer before all other operation
+	  Configuration configuration = *(Configuration*) c.data;
+    printParameters(configuration);
 
-    /*
-    pinMode(2, INPUT);
-    pinMode(3, INPUT);
-    */
+    
+    // pinMode(2, OUTPUT);
+    // pinMode(3, OUTPUT);
+    
 
     pinMode(7, INPUT);
     pinMode(6, INPUT);
@@ -160,11 +181,36 @@ void loop() {
 
         if((millis() - delays[i]) > 50){
           delays[i] = millis();
+
+          //////// TRANSMITION TEST ////////
+          // ResponseStatus rs2 = lora.sendMessage("Hello World?");
+          // packet msg(0xFF, s.id_, 0xFE);
+          // ResponseStatus rs = lora.sendMessage(&msg, sizeof(struct packet));
+          // Serial.println(rs.code);
+          // Serial.println(rs.getResponseDescription());
+          //////// TEST ////////
           
           int state1 = s.s1_.sensor.ping_cm();
-          Serial.println(state1);
           int state2 = s.s2_.sensor.ping_cm();
-          Serial.println(state2);
+
+          //////// DEBUG PRINT ////////
+          // if(s.id_ == 1){
+          //   Serial.println(state1);
+          //   Serial.println(state2);
+          // }
+          //////// DEBUG PRINT ////////
+          //////// DEBUG PRINT ////////
+          // if(state1 != 0){
+          //   char str[50];
+          //   sprintf(str, "ID: %d, sensore: 1, valore: %d", s.id_, state1);  // Formattiamo la stringa
+          //   Serial.println(str);
+          // }
+          // if(state2 != 0){
+          //   char str[50];
+          //   sprintf(str, "ID: %d, sensore: 2, valore: %d", s.id_, state2);  // Formattiamo la stringa
+          //   Serial.println(str);
+          // }
+
 
           if(((state1 <= ACTIVATION_TRESH) && (state1 != 0)) && ((state2 <= ACTIVATION_TRESH) && (state2 != 0))){
             state1 = HIGH;
@@ -173,35 +219,35 @@ void loop() {
 
           if (state1 == HIGH) {
             s.s1_.state = on_;
-            if (to_stabilize){
-              stabilize = millis();
-              to_stabilize = false;
+            if (to_stabilize[i]){
+              stabilize[i] = millis();
+              to_stabilize[i] = false;
             }
           }
           else{
             s.s1_.state = off_;
-            stabilize = millis();
-            to_stabilize = true;
+            stabilize[i] = millis();
+            to_stabilize[i] = true;
             
           }
           if (state2 == HIGH) {
             s.s2_.state = on_;
-            if (to_stabilize){
-              stabilize = millis();
-              to_stabilize = false;
+            if (to_stabilize[i]){
+              stabilize[i] = millis();
+              to_stabilize[i] = false;
             }
           }
           else{
             s.s2_.state = off_;
-            stabilize = millis();
-            to_stabilize = true;
+            stabilize[i] = millis();
+            to_stabilize[i] = true;
           }
           
 
           // state change
           // default behaviour is staying in the current state
           s.futureState = s.currentState;
-          if((s.currentState == free_) && s.s1_.state == on_ && s.s2_.state == on_ && ((millis() - stabilize) > 1500)){
+          if((s.currentState == free_) && s.s1_.state == on_ && s.s2_.state == on_ && ((millis() - stabilize[i]) > 1500)){
               s.futureState = taken_;
           }
           else if(s.currentState == taken_ && s.s1_.state == off_ && s.s2_.state == off_){
@@ -214,10 +260,11 @@ void loop() {
             Serial.print(s.currentState);
             Serial.print(" a ");
             Serial.println(s.futureState);
-            stabilize = millis();
-            to_stabilize = true;
+            stabilize[i] = millis();
+            to_stabilize[i] = true;
 
             packet msg(0xFF, s.id_, 0xFE);
+            Serial.println("DEBUG: STO MANDNANDO IL MESSAGGIO");
             ResponseStatus rs = lora.sendMessage(&msg, sizeof(struct packet));
             Serial.println(rs.getResponseDescription());
           }
@@ -249,7 +296,7 @@ void loop() {
 
         
         //////////////////////////////////////////////////////////////////////////////
-        ///DEBUG VERSION (SOLO 1 SENSORE)
+        ///DEBUG VERSION (SOLO 1 SENSORE) Da modificare
         //////////////////////////////////////////////////////////////////////////////
 
         /*
@@ -263,19 +310,19 @@ void loop() {
         if (state1 == HIGH) {
           s.s1_.state = on_;
           if (to_stabilize){
-            stabilize = millis();
-            to_stabilize = false;
+            stabilize[i] = millis();
+            to_stabilize[i] = false;
           }
         }
         else{
           s.s1_.state = off_;
-          stabilize = millis();
-          to_stabilize = true;
+          stabilize[i] = millis();
+          to_stabilize[i] = true;
           
         }
 
         s.futureState = s.currentState;
-        if((s.currentState == free_) && s.s1_.state == on_ && ((millis() - stabilize) > 1500)){
+        if((s.currentState == free_) && s.s1_.state == on_ && ((millis() - stabilize[i]) > 1500)){
             s.futureState = taken_;
         }
         else if(s.currentState == taken_ && s.s1_.state == off_){
@@ -288,8 +335,8 @@ void loop() {
           Serial.print(s.currentState);
           Serial.print(" a ");
           Serial.println(s.futureState);
-          stabilize = millis();
-          to_stabilize = true;
+          stabilize[i] = millis();
+          to_stabilize[i] = true;
           packet msg(0xFF, s.id_, 0xFE);
           //ResponseStatus rs = lora.sendMessage(String(s.id_));
           ResponseStatus rs = lora.sendMessage(&msg, sizeof(struct packet));
@@ -310,5 +357,32 @@ void loop() {
         s.currentState = s.futureState;
         */
     }
+}
 
+
+void printParameters(struct Configuration configuration) {
+  Serial.println("----------------------------------------");
+
+  Serial.print(F("HEAD : "));  Serial.print(configuration.COMMAND, HEX);Serial.print(" ");Serial.print(configuration.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(configuration.LENGHT, HEX);
+  Serial.println(F(" "));
+  Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, HEX);
+  Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, HEX);
+  Serial.println(F(" "));
+  Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
+  Serial.println(F(" "));
+  Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
+  Serial.print(F("SpeedUARTDatte     : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRateDescription());
+  Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRateDescription());
+  Serial.println(F(" "));
+  Serial.print(F("OptionSubPacketSett: "));  Serial.print(configuration.OPTION.subPacketSetting, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getSubPacketSetting());
+  Serial.print(F("OptionTranPower    : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
+  Serial.print(F("OptionRSSIAmbientNo: "));  Serial.print(configuration.OPTION.RSSIAmbientNoise, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getRSSIAmbientNoiseEnable());
+  Serial.println(F(" "));
+  Serial.print(F("TransModeWORPeriod : "));  Serial.print(configuration.TRANSMISSION_MODE.WORPeriod, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
+  Serial.print(F("TransModeEnableLBT : "));  Serial.print(configuration.TRANSMISSION_MODE.enableLBT, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
+  Serial.print(F("TransModeEnableRSSI: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRSSI, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
+  Serial.print(F("TransModeFixedTrans: "));  Serial.print(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
+
+
+  Serial.println("----------------------------------------");
 }
